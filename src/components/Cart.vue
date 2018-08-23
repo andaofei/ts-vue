694c474b3dba2b776b5f33dbe33480cebad6c2b5
zhuaofei <2353536563@qq.com>
2018-08-23 10:08:18 +0800

    init

---
+++ src/components/Cart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="cart">
+        <h2>cart</h2>
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Cart'
+})
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 @click="onChildChanged(msg)">Essential Links</h2>
+    <Cart/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator'
+import Cart from '@/components/Cart.vue'
+@Component({
+  components: {
+    Cart
+  }
+})
+export default class Helloworld extends Vue {
+  msg: string = '123'
+  @Watch('child')
+  onChildChanged (val: string, oldVal: string) {
+    this.msg = '89565'
+    console.log(this.msg)
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
